cff6b6d3ed7e15feb7a442f6fed833f1dfd2739b
Scheppach Amadeu <Amadeu.Scheppach@cognizant-mobility.com>
2023-06-17 13:54:52 +0200

    Added endpoints to enable communication between backend and frontend.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask.helpers import send_from_directory, send_file
 # from flask_cors import CORS, cross_origin
 from job_resume_matching_phraseMatcher import compare_resumes_to_job, sort_applicants, create_plot_resume_to_job
@@ -17,14 +17,24 @@ def index():
 def get_skill_plot():
     resume_directory = 'C:/Users/SEPA/lanchain_ir2/Resume_data_pdf'  # enter your path here where you saved the resumes
     skill_file = 'C:/Users/SEPA/lanchain_ir2/job_demand.xlsx'
-    important_Corresponding skill catalogue = ['NLP', 'MLOps']
+    skills = ['NLP', 'MLOps']
 
     new_data = compare_resumes_to_job(resume_directory, skill_file)
-    new_data = sort_applicants(new_data, important_Corresponding skill catalogue)
+    new_data = sort_applicants(new_data, skills)
     create_plot_resume_to_job(new_data)
 
     return send_file('skill_plot.png', mimetype='image/png')
 
+skill_list = []
+@app.route('/skillList', methods=['POST'])
+def add_skill():
+    new_skill = request.json
+    skill_list.append(new_skill)
+    return 'Skill added successfully', 200
+
+@app.route('/skillList', methods=['GET'])
+def get_skill_list():
+    return {'skillList': skill_list}, 200
 
 #@app.route('/')
 #def serve():
--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/compare_Corresponding skill catalogue" target="_blank" rel="noopener noreferrer">Resume Extraction and Comparison</a></li>
-        <li><a href="/ResumeMatching">Resume Job Matching</a></li>
-        <li><a href="/Resume Mining">Resume Mining</a></li>
+      <ul style={{ display: 'flex', listStyleType: 'none' }}>
+        <li style={{ marginRight: '10px' }}><a href="/">Home</a></li>
+        <li style={{ marginRight: '10px' }}><a href="/compare_Corresponding skill catalogue" target="_blank" rel="noopener noreferrer">Search for Skill Sets</a></li>
+        <li style={{ marginRight: '10px' }}><a href="/ResumeMatching">Resume Job Matching</a></li>
+        <li style={{ marginRight: '10px' }}><a href="/Resume Mining">Resume Mining</a></li>
         <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
